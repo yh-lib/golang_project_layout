8027857e3952411ea9e1d9ea52f2d0622f6eef2d
yaohui.li <liyaohuiisgreat@gmail.com>
2026-02-16 14:30:15 +0800

    add gin.

--- config/config.go
+++ config/config.go
@@ -1,2 +1,8 @@
 // 配置层 存放程序的配置信息
 package config
+
+import "fmt"
+
+func init() {
+	fmt.Println("开始加载程序配置")
+}
--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 // 项目的总入口
 package main
 
-func main() {
+import (
+	_ "golang_project_layout/config"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 1. 开始加载程序配置
+	// 2. 配置 gin
+	r := gin.Default()
+	r.Run()
 }
